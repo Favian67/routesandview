97c454c844aebe9d5ace02c7b1421c8c5b6bd645
Favian67 <viantama67@gmail.com>
2022-01-18 12:51:41 +0700

    KK4A

---
+++ perpustakaan/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Favian | About</title>
+</head>
+<body>
+    <h1> Halaman About </h1>
+    <h3> Favian </h3>
+    <p> 3103120090@student.smktelkom-pwt.sch.id </p>
+    <img src="images/foto.jpeg" alt="halo" width="200">
+</body>
+</html>
